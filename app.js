5ff9ea9f2f6cd9dcd8e1ebafd3b80f5e1a93173c
Prakrisht Dahiya <63427355+beetrandahiya@users.noreply.github.com>
2022-07-02 20:54:45 +0530

    finally something working

--- app.js
+++ app.js
@@ -10,17 +10,30 @@ function runJS() {
 //set default value for js
 function setDefaultJS() {
     document.getElementById("js").value
-        = " elem = document.getElementById('output');\n" +
-        " var t=0;\n" +
-        " setCanvas(elem);\n" +
-        " function draw(){\n" +
-        "     clearCanvas();\n" +
-        "    new point(200+100*sin(t),200+100*cos(t),'#e9d4ff',10);\n" +
-        "    new point(200,200,'#f4cfdf',20);\n" +
-        "    requestAnimationFrame(draw);\n" +
-        "    t+=0.1;\n" +
-        "    }\n" +
-        " draw();\n" ;
+        = ` elem = document.getElementById('output');
+        var t=0;
+        setCanvas(elem);
+       var points1=[];
+       var points2=[];
+        function draw(){
+            clearCanvas();
+       x=WIDTH/2+ (200*cos(t))/(1+sin(t)**2)
+       y=HEIGHT/2+ (200*sin(t)*cos(t))/(1+sin(t)**2)
+       points1.push([x,y]);
+       points2.push([x+50*sin(t),y+50*cos(t)]);
+           new point(x+50*sin(t),y+50*cos(t),'#e9d4ff',5);
+           new point(x,y,'#f4cfdf',10);
+       if(points1.length>200){
+       points1.shift()}
+       if(points2.length>200){
+       points2.shift()}
+       new polygon(points1, "#fff", 0, "#f4cfdf", 3, false);
+       new polygon(points2, "#fff", 0, "#e9d4ff", 1, false);
+           requestAnimationFrame(draw);
+           t+=0.01;
+           }
+        draw();
+       `
 }
 
 setDefaultJS();
@@ -29,3 +42,8 @@ setDefaultJS();
 document.getElementById("run-js").addEventListener("click", runJS);
 
 frameRate("#fps");
+
+window.onbeforeunload = function(event)
+{
+    return confirm("Confirm refresh");
+};
